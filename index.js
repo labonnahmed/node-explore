cfe29e3aee2510fe523424e2710f5ff8bb76e28e
labonnahmed <labonnoakter650@gmail.com>
2023-06-18 10:23:52 +0600

    added a new api

--- index.js
+++ index.js
@@ -7,16 +7,17 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-    const fruits= {item1: "mango", item2:"banana", item3:"orange", item4:"licchi"}
+    const fruits = { item1: "mango", item2: "banana", item3: "orange", item4: "licchi" }
     res.send(fruits);
 });
 
-// const fruits= ['mango', 'banana', 'orange', 'licchi', 'jack-fruit'];
-// app.get('/fruits/:item', (req, res) => {
-//     const item= req.params.item;
-//     const fruit= fruits[item];
-//     res.send({name:fruit, id:item})
-// });
+
+app.get('/fruits/:item', (req, res) => {
+    const fruits = ['mango', 'banana', 'orange', 'licchi', 'jack-fruit'];
+    const item = req.params.item;
+    const fruit = fruits[item];
+    res.send({ name: fruit, id: item })
+});
 
 
 // //post

e570c8065c423e527326f92f60101a1d7eaa73fb
labonnahmed <labonnoakter650@gmail.com>
2023-06-18 10:12:38 +0600

    some changes

--- index.js
+++ index.js
@@ -11,18 +11,18 @@ app.get('/', (req, res) => {
     res.send(fruits);
 });
 
-const fruits= ['mango', 'banana', 'orange', 'licchi', 'jack-fruit'];
-app.get('/fruits/:item', (req, res) => {
-    const item= req.params.item;
-    const fruit= fruits[item];
-    res.send({name:fruit, id:item})
-});
+// const fruits= ['mango', 'banana', 'orange', 'licchi', 'jack-fruit'];
+// app.get('/fruits/:item', (req, res) => {
+//     const item= req.params.item;
+//     const fruit= fruits[item];
+//     res.send({name:fruit, id:item})
+// });
 
 
-//post
-app.post('/addFruits', (req, res) => {
-    //save to database
-    res.send(req.body);
-})
+// //post
+// app.post('/addFruits', (req, res) => {
+//     //save to database
+//     res.send(req.body);
+// })
 
 app.listen(4000, () => console.log('listenting to port 4000'));
